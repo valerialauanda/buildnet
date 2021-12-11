a256a0eef4c16cd1c350a81e42ee253582617330
valerialauanda <91025814+valerialauanda@users.noreply.github.com>
2021-12-11 05:19:59 -0300

    Add files via upload

---
+++ build/precache-manifest.1ed4a129b43dc0708eca75ff3494abe5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b86bd4473d3a6b5a42d180dc7079e723",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8030791af35b86645ab1",
+    "url": "/static/css/main.12f95472.chunk.css"
+  },
+  {
+    "revision": "512c35c7a251e4d8c78d",
+    "url": "/static/js/2.2a292687.chunk.js"
+  },
+  {
+    "revision": "00e2b2cdc2475b76a408969f1af93df0",
+    "url": "/static/js/2.2a292687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8030791af35b86645ab1",
+    "url": "/static/js/main.89b2d428.chunk.js"
+  },
+  {
+    "revision": "ec2d86833d8c53194aa2",
+    "url": "/static/js/runtime-main.d4058e28.js"
+  },
+  {
+    "revision": "d36f781970e76d2cc36bdc6eb50754d5",
+    "url": "/static/media/logo.d36f7819.jpg"
+  }
+]);
